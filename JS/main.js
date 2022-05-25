7d5d31c2ad3cc8774a3de3363b157cc9a8590848
ProNatalya <natarich18@gmail.com>
2022-05-25 23:17:06 +0300

    load

--- JS/main.js
+++ JS/main.js
@@ -3,6 +3,6 @@ $(function(){
     $('.bike-slider').slick({
         arrows: false,
         dots: true
-    });/*теперь previous и next нет, появились квадратики 1-5 внизу картинок*/
+    });
     
 });
